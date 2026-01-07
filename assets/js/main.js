900fb39235f87fb144189edeb109db59c4b01020
ChristianSangy <chrisanchieta23@gmail.com>
2026-01-07 13:00:51 -0300

    Primeiro commit

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+           
+            <div class="info">
+                <div class="sobre">
+                    <strong>Sobre</strong><br>
+                    Altura: ${pokemon.height} <br>
+                    Peso: ${pokemon.weight} <br>
+                    Experiência Base: ${pokemon.base_experience}
+                </div>
+            
+                <div class="habilidades">
+                    <strong>Habilidades</strong><br>
+                    ${pokemon.abilities.map((ability) => `<span class="ability">${ability}</span>`).join(', ')}
+                </div>
+            </div>
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
